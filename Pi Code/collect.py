53d32555f1a68ff275e910b6a3a0cae62ba2ba0f
Alexander Sun <technoal9@gmail.com>
2019-11-30 21:06:54 -0500

    added serial code

--- Pi Code/collect.py
+++ Pi Code/collect.py
@@ -1,11 +1,54 @@
 import serial
+from datetime import datetime
 
 #setup serials for the testing rig and microphone array
-TR = serial.Serial('/dev/ttyACMO', 9600)
-MA = serial.Serial('/dev/ttyACMO', 9600)
+TRport = '/dev/ttyACMO'
+MAport = '/dev/ttyACMO'
 
-def collect(angle, pitch):
+check = 'ch'
 
+def collect(angle, increment):
+    # for each angle, send angle to TR
+    # wait for response from TR
+    # once received, if TR went to specified angle
+    bool TRres = serialTR(angle, True)
+    if TRres:
+        return
+
+    # send bit to MA, wait for response
+    # once received
+ 
+
+def serialTR(message, needsCheck, port, expected):
+    bool quit = False
+    with serial.Serial(port, 9600) as ser:
+        ser.write(bin(message))
+        print('--------------------------')
+        print('-----TR Message SENT!-----')
+        print('--------------------------')
+        if needsCheck:
+            SERprimerBit = ser.read(timeout = 5)
+            if SERprimerBit == None:
+                print('--------------------------')
+                print('----TR Receiving ERROR----')
+                print('--------------------------')
+                quit = True
+            elif SERprimerBit != expected:
+                print('--------------------------')
+                print('----??????????????????----')
+                print('--------------------------')
+                quit = True
+            else
+                quit = False
+        else:
+            quit = False
+    return quit
 
 if __name__ == '__init__':
-    collect()
+
+
+#tasks
+
+#send priming bit to MA and testing rig, telling them to go
+#then wait for responding data on the arduino to come back
+#once received, restart
